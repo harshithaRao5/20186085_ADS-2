0b583f3a0ae79626102311af5b4ca8eacfcfc81f
harshithaRao5 <harshithanallani@msitprogram.net>
2018-11-01 19:12:05 +0530

    "ads2_m4_a1_1: 0/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2 assignments/m4/Code Camp - WordNet/Code Camp - WordNet/WordNet/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.io.File;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused constructor.
+	}
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		String synset = StdIn.readString();
+		System.out.println(synset);
+		String hypernym = StdIn.readString();
+		String type = StdIn.readString();
+
+		if (type.equals("Graph")) {
+			WordNet wordnet = new WordNet(synset, hypernym);
+
+			//System.out.println(wordnet.);
+		} else {
+
+		}
+
+	}
+}
---
+++ ADS-2 assignments/m4/Code Camp - WordNet/Code Camp - WordNet/WordNet/WordNet.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+public class WordNet {
+    private LinearProbingHashST<String, Integer> linearprobing;
+    private Digraph digraph;
+
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms) {
+        linearprobing = new LinearProbingHashST<String, Integer>();
+        int synsetV = readSynset(synsets);
+        digraph = readHypernym(hypernyms, synsetV);
+    }
+    public int readSynset(String synset) {
+        In in = new In("./Files/" + synset);
+        int vertices = 0;
+        while (!in.isEmpty()) {
+            vertices++;
+            String[] tokens = in.readString().split(",");
+            int id = Integer.parseInt(tokens[0]);
+            String[] nouns = tokens[1].split(" ");
+            for (String each : nouns) {
+                linearprobing.put(each, id);
+            }
+
+
+        }
+        return vertices;
+
+
+    }
+    public Digraph readHypernym(String hypernym, int synsetv) {
+        In in = new In("./Files/" + hypernym);
+        Digraph graph = new Digraph(synsetv);
+        while (!in.isEmpty()) {
+            String[] tokens = in.readString().split(",");
+            int hyponyms = Integer.parseInt(tokens[0]);
+            for (int i = 0; i < tokens.length; i++) {
+                int hypernyms = Integer.parseInt(tokens[1]);
+                graph.addEdge(hyponyms, hypernyms);
+            }
+        }
+        System.out.println(graph);
+        return graph;
+    }
+
+
+    // returns all WordNet nouns
+    // public Iterable<String> nouns() {
+
+    // }
+
+    // // is the word a WordNet noun?
+    // public boolean isNoun(String word) {
+
+    // }
+
+    // // distance between nounA and nounB (defined below)
+    // public int distance(String nounA, String nounB) {
+
+    // }
+
+    // // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // // in a shortest ancestral path (defined below)
+    // public String sap(String nounA, String nounB) {
+
+    // }
+
+    // // do unit testing of this class
+    // public static void main(String[] args) {
+
+    //}
+
+
+
+
+}
