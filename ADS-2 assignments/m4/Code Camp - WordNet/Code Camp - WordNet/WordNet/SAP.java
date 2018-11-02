d0a7ca5005f088ab02caf6a59ee1678531fc9a5e
harshithaRao5 <harshithanallani@msitprogram.net>
2018-11-02 18:19:20 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m4/Code Camp - WordNet/Code Camp - WordNet/WordNet/SAP.java
+++ ADS-2 assignments/m4/Code Camp - WordNet/Code Camp - WordNet/WordNet/SAP.java
@@ -9,6 +9,7 @@ public class SAP {
     }
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        ancestor(v, w);
         return distanceGlobal;
     }
 
@@ -16,14 +17,14 @@ public class SAP {
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
         BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(graph, v);
         BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(graph, w);
-        int shortPath = Integer.MAX_VALUE;
+        distanceGlobal = Integer.MAX_VALUE;
         int ancestors = -1;
         for (int i = 0; i < graph.V(); i++) {
             if (bfsV.hasPathTo(i) && bfsW.hasPathTo(i)) {
                 int distance = bfsV.distTo(i) + bfsW.distTo(i);
-                if (distance < shortPath) {
-                    shortPath = distance;
-                    distanceGlobal = shortPath;
+                if (distance < distanceGlobal) {
+                    //shortPath = distance;
+                    distanceGlobal = distance;
                     ancestors = i;
                 }
                 // return ancestor;
