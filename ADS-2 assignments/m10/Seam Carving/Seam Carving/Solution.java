72fe0607e645129c72fcba1d09fac8dffd00e522
harshithaRao5 <harshithanallani@msitprogram.net>
2018-11-11 18:24:25 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m10/Seam Carving/Seam Carving/Solution.java
+++ ADS-2 assignments/m10/Seam Carving/Seam Carving/Solution.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.Arrays;
-
+/**
+ * client class.
+ */
 public final class Solution {
     /**
      * Constructs the object.
@@ -23,8 +25,9 @@ public final class Solution {
         StdOut.printf("Printing energy calculated for each pixel.\n");
 
         for (int row = 0; row < sc.height(); row++) {
-            for (int col = 0; col < sc.width(); col++)
+            for (int col = 0; col < sc.width(); col++) {
                 StdOut.printf("%9.0f ", sc.energy(col, row));
+            }
             StdOut.println();
         }
     }
@@ -43,8 +46,8 @@ public final class Solution {
             for (int col = 0; col < carver.width(); col++) {
                 double energy = carver.energy(col, row);
                 String marker = " ";
-                if ((direction == true && row == seam[col]) ||
-                        (direction == false   && col == seam[row])) {
+                if ((direction == true && row == seam[col])
+                    || (direction == false   && col == seam[row])) {
                     marker = "*";
                     totalSeamEnergy += energy;
                 }
@@ -148,7 +151,7 @@ public final class Solution {
                 break;
             }
         } catch (Exception ex) {
-        	//ex.printStackTrace();
+            //ex.printStackTrace();
             System.out.println(ex.getMessage());
         }
     }
