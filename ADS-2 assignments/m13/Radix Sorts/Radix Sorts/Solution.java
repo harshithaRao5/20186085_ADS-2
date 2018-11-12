dd5bbe1a8a4108ad67c796da0578d15e012f90c2
harshithaRao5 <harshithanallani@msitprogram.net>
2018-11-12 14:39:52 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m13/Radix Sorts/Radix Sorts/Solution.java
+++ ADS-2 assignments/m13/Radix Sorts/Radix Sorts/Solution.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * client method.
+ */
 public final class Solution {
-	private Solution() {
-
-	}
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		String[] strings = new String[n];
-		for(int i = 0; i < n;i++) {
-			strings[i] = sc.nextLine();
-		}
-		LSD lsdobj = new LSD();
-		lsdobj.sort(strings, strings[0].length());
-		System.out.println(Arrays.toString(strings));
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused constructor.
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = sc.nextLine();
+        }
+        LSD lsdobj = new LSD();
+        lsdobj.sort(strings, strings[0].length());
+        System.out.println(Arrays.toString(strings));
+    }
 }
