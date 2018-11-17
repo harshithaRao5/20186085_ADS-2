66cabaf16670d86f5771d8996eec0dcac09a0f0d
harshithaRao5 <harshithanallani@msitprogram.net>
2018-11-17 13:23:21 +0530

    "ads2_m18_a1_1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m18/ADS-2Exam-3/ADS-2Exam-3/Solution.java
+++ ADS-2 assignments/m18/ADS-2Exam-3/ADS-2Exam-3/Solution.java
@@ -14,7 +14,7 @@ public final class Solution {
 
     }
     /**
-     * { function_description }
+     * { function_description }.
      *
      * @param      args  The arguments
      */
@@ -96,7 +96,7 @@ public final class Solution {
     }
 
     /**
-     * { function_description }
+     * { function_description }.
      *
      * @param      file  The file
      *
